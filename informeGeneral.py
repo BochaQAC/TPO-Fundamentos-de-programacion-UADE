40edea09f2c17d00a3a084c5e52df75802b8d70e
Bautista <bautistamiano@gmail.com>
2026-06-17 22:03:06 -0300

    Modificacion en el metodoSecuencial

--- informeGeneral.py
+++ informeGeneral.py
@@ -18,23 +18,19 @@ def informeGeneral(peliculas):
 def ordenar_peliculas_por_seleccion(peliculas):
     '''Ordena las películas usando el MÉTODO DE SELECCIÓN'''
 
-    n = len(peliculas)
-
-    # Metodo Selección
-    for i in range(n - 1):
-        pos_max = i
-        for j in range(i + 1, n):
-            # Si la película en j tiene MÁS entradas que la que está en pos_max
-            if peliculas[j][4] > peliculas[pos_max][4]:
-                pos_max = j
-                # Si tienen igual cantidad de entradas, ordenar alfabéticamente
-            elif peliculas[j][4] == peliculas[pos_max][4]:
-                # Orden ascendente (A-Z) por título
-                if peliculas[j][0] < peliculas[pos_max][0]:
-                    pos_max = j
-        # Intercambiar la película en pos_max con la película en i
-        if pos_max != i:
-            peliculas[i], peliculas[pos_max] = peliculas[pos_max], peliculas[i]    
+    for i in range(len(peliculas)):
+        for j in range(i+1,len(peliculas)):
+            if peliculas[i][4] < peliculas[j][4]:
+                aux = peliculas[i]
+                peliculas[i] = peliculas[j]
+                peliculas[j] = aux
+                
+            elif peliculas[i][4] == peliculas [j][4]:
+                if peliculas[i][0] > peliculas[j][0]:
+                    aux = peliculas[i]
+                    peliculas[i] = peliculas[j]
+                    peliculas[j] = aux
+    return peliculas
 
 def mostrar_informe(peliculas):
     '''Muestra el informe con todos los datos de las películas en reporte matricial.'''   
